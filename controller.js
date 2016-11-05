65469e6e7de2aed7cbf199609fee8e0fb3208858
sqro2 <sqro2project@gmail.com>
2016-11-06 00:57:01 +0530

    added code_gen module

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
- var fs = require('fs');
  var path = require('path');
  var parser = require(path.join(__dirname,'/cust_modules/parser.js'));
+ var code_gen = require(path.join(__dirname,'/cust_modules/code_gen.js'));
  parser.init(process.argv);
+ code_gen.init();
